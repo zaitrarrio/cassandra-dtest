f6fc25bb2dc562c744c4a9cf7879f7ed47b3b7ab
Ryan McGuire <ryan@enigmacurry.com>
2014-03-10 14:43:18 -0400

    Adds deletes to 6285 test

--- test_6285.py
+++ test_6285.py
@@ -6,6 +6,7 @@ import uuid
 import os
 import pycassa
 import threading
+import random
 
 JNA_PATH = '/usr/share/java/jna/jna.jar'
 
@@ -22,18 +23,35 @@ class CompactionTimer(threading.Thread):
 
 
 class RowWriter(threading.Thread):
-    def __init__(self, num_rows):
-        self.num_rows = num_rows
+    def __init__(self, row_start, row_stop):
+        self.row_start = row_start
+        self.row_stop = row_stop
+        self.num_rows = row_stop - row_start - 1
         threading.Thread.__init__(self)
         
     def run(self):
         pool = pycassa.ConnectionPool('test')
         cf = pycassa.ColumnFamily(pool, 'CF')
         debug("Thread %s starting write of %d rows..." % (threading.current_thread(), self.num_rows))
-        for x in xrange(self.num_rows):
+        for x in xrange(self.row_start, self.row_stop):
             cf.insert(str(x), {datetime.datetime.utcnow(): 'blah'})
         debug("Thread %s done writing" % (threading.current_thread(),))
         
+
+class RowDeleter(threading.Thread):
+    def __init__(self, row_keys, delay_between_deletes=0):
+        self.row_keys = row_keys
+        self.delay_between_deletes = delay_between_deletes
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        pool = pycassa.ConnectionPool('test')
+        cf = pycassa.ColumnFamily(pool, 'CF')
+        debug("Thread %s starting deletes ..." % (threading.current_thread(),))
+        for x in self.row_keys:
+            time.sleep(self.delay_between_deletes)
+            cf.remove(str(x))
+        debug("Thread %s done deleting" % (threading.current_thread(),))
 
 
 class Test6285(Tester):
@@ -77,27 +95,46 @@ class Test6285(Tester):
   default_time_to_live=0 AND
   speculative_retry='NONE' AND
   memtable_flush_period_in_ms=0 AND
-  compaction={'class': 'SizeTieredCompactionStrategy'} AND
+  compaction={'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb' : 2} AND
   compression={'chunk_length_kb': '64', 'sstable_compression': 'DeflateCompressor'};
 """)
 
 
-        threads = [RowWriter(100000) for r in range(10)]
-        for t in threads:
-            t.start()
-        compactors = [CompactionTimer(node1, 10),
-                      CompactionTimer(node2, 12),
-                      CompactionTimer(node1, 30),
-                      CompactionTimer(node2, 32)]
-        for t in compactors:
-            t.start()
+        def start_writes(num_threads):
+            write_threads = []
+            rows_per_thread = 1000
+            rows = 0
+            for t in range(num_threads):
+                t = RowWriter(rows, rows+rows_per_thread)
+                write_threads.append(t)
+                rows += rows_per_thread
+                t.start()
 
-        for t in threads:
-            t.join()
-        for t in compactors:
-            t.join()
+            return write_threads, rows
+
+        def start_deletes(num_threads, max_row):
+            delete_threads = []
+            for t in range(num_threads):
+                  t = RowDeleter([random.randint(0,max_row) for x in xrange(100)])
+                  delete_threads.append(t)
+                  t.start()
+
+            return delete_threads
+
+
+        # Do an initial big write:
+        write_threads, first_write_rows = start_writes(50)
+        # Delete from the first write at the same time:
+        delete_threads = start_deletes(10, first_write_rows)
         
-        node1.compact()
+        for t in write_threads:
+            t.join()
+        for t in delete_threads:
+            t.join()
+
+        cluster.compact()
+
+        node2.stop()
+        node2.start()
         node1.stop()
         node1.start()
-
