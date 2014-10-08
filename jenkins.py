e1c86f6c016b2b16833c6e0d4f49ea511042c6ce
Philip Thompson <philip.thompson@datastax.com>
2014-10-08 14:26:51 -0500

    Remember to run one test at a time

--- jenkins.py
+++ jenkins.py
@@ -2,7 +2,7 @@ import subprocess, os, sys, psutil, time
 
 current_dir = sys.argv[1]
 for test in os.listdir(current_dir):
-    args = ['nosetests', '--nocapture', '--verbosity=3', '--with-xunit', '--xunit-file=%s.xml' % test]
+    args = ['nosetests', '--nocapture', '--verbosity=3', '--with-xunit', '--xunit-file=%s.xml' % test, test]
     p = subprocess.Popen(args)
     process = psutil.Process(p.pid)
     gone, alive = psutil.wait_procs([process], 800)
